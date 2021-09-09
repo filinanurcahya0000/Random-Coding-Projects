abd086a87781877d9b332760cc90002ac4adbb51
Filina Nurcahya-Tjoa <83178463+filinanurcahya0000@users.noreply.github.com>
2021-09-08 22:24:47 -0500

    Add files via upload

---
+++ Monty Hall Game.R
@@ -0,0 +1,40 @@
+# Function to run through Monty Hall game.
+resultslist <- c()
+montyhall <- function(x) {
+    for(i in x) {
+    chosenbyplayer <- sample(1:3,1)
+    winningdoor <- sample(1:3,1)
+    hostoptions <- sample(1:3)
+    chosenbyhost <- setdiff(hostoptions,seq(chosenbyplayer,winningdoor))
+    if (winningdoor == chosenbyplayer) {
+        resultslist = c(resultslist, "Stay")}
+    else {
+        resultslist = c(resultslist, "Switch")}
+    }
+    return(resultslist)
+}
+# Function to run through Monty Hall game for n times.
+n <- c(seq(100,10000,10))
+
+list1 <- data.frame()
+for (x in n) {
+    table1 <- table(montyhall(1:x))
+    proptable <- as.vector(prop.table(table1))
+    proptable <- split(proptable,c(1,2))
+    positions <- nrow(list1) + 1
+    list1[positions,1] <- x 
+    list1[positions,2:3] <- proptable }
+
+library(ggplot2)
+
+ggplot() + geom_line(aes(x = list1$V1, y = list1$`1`, color = "Stay")) + geom_line(aes(x = list1$V1, y = list1$`2`, color = "Switch")) + ylab("Probability of Winning") + xlab("Number of Trials") +
+    scale_color_manual(name = "Player's Choice", values = c("Switch" = "turquoise1", "Stay" = "palegreen3")) + ggtitle("Monty Hall Game Simulation")
+
+# A single run through of the game.
+print(montyhall(1))
+
+# A hundred run throughs of the game. 
+hundredtrials <- montyhall(1:100)
+table <- table(hundredtrials)
+prop.table(table)
+
